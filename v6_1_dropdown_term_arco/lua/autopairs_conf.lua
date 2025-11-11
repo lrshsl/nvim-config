a9f141c219343e61baa03ff0929b067977e49eda
lrshsl <hoeslilars@gmail.com>
2025-11-11 21:00:48 +0100

    Configure autopairs for $$ in typ files

--- v6_1_dropdown_term_arco/lua/autopairs_conf.lua
+++ v6_1_dropdown_term_arco/lua/autopairs_conf.lua
@@ -1,29 +1,37 @@
 return {
-	'windwp/nvim-autopairs',
-	event = "InsertEnter",
-	opts = {
-		-- check_ts = true,
-		ignored_next_char = "[%w%.]",
-		enable_check_bracket_line = true, -- check bracket in same line
+   'windwp/nvim-autopairs',
+   event = "InsertEnter",
+   opts = {
+      -- check_ts = true,
+      ignored_next_char = "[%w%.]",
+      enable_check_bracket_line = true, -- check bracket in same line
 
-		fast_wrap = {
-			map = '<C-k>',
-			chars = { '{', '[', '(', '"', "'" },
-			pattern = [=[[%'%"%>%]%)%}%,]]=],
-			end_key = '$',
-			before_key = 'h',
-			after_key = 'i',
-			cursor_pos_before = true,
-			keys = 'qwertyuiopzxcvbnmasdfghjkl',
-			manual_position = true,
-			highlight = 'Search',
-			highlight_grey = 'Comment'
-		},
-	},
-	config = function()
-		local npairs = require 'nvim-autopairs'
-		npairs.setup()
+      fast_wrap = {
+         map = '<C-k>',
+         chars = { '{', '[', '(', '"', "'" },
+         pattern = [=[[%'%"%>%]%)%}%,]]=],
+         end_key = '$',
+         before_key = 'h',
+         after_key = 'i',
+         cursor_pos_before = true,
+         keys = 'qwertyuiopzxcvbnmasdfghjkl',
+         manual_position = true,
+         highlight = 'Search',
+         highlight_grey = 'Comment'
+      },
+   },
+   config = function()
+      local npairs = require 'nvim-autopairs'
+      local Rule = require 'nvim-autopairs.rule'
+      local cond = require 'nvim-autopairs.conds'
+      npairs.setup()
 
-		npairs.get_rules("'")[1].not_filetypes = { "scheme", "lisp", "racket" }
-	end,
+      npairs.get_rules("'")[1].not_filetypes = { 'rust', 'scheme', 'lisp', 'racket' }
+      npairs.add_rules(
+         {
+            Rule('$', '$', { 'typ', 'typst', 'tex', 'latex' })
+                :with_move(cond.always)
+         }
+      )
+   end,
 }
